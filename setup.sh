eb324884b8cf7abaa42d0036aa00a09903dc35fb
Ray Garner <ray@perfectcast.co.uk>
2019-12-26 19:26:23 +0000

    first

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#install packages
+pacman -S zathura zathura-pdf-mupdf cmus make gvim gnu-free-fonts xorg firefox redshift youtube-dl feh mpv scrot xbindkeys htop sudo vi pavucontrol pulseaudio compton acpi git xorg-init xorg-xsetroot xorg-server dmenu libx11 freetype2 pkg-config transmission-cli base-devel kolourpaint screenfetch
+
+#set keyboard layout
+localectl --no-convert set-x11-keymap gb
+
+#add user
+useradd ray
+passwd ray
+usermod -aG wheel ray
+
+#switch to new user
+su ray
+
+#cd to home dir
+cd /home/ray
+
+#create dirs
+mkdir pics pics/screenshots pics/papes vids vids/films programming music docs builds
+
+#set up git
+git config --global user.email "ray@perfectcast.co.uk"
+git config --global user.name "Ray Garner"
+
+#download wallpaper
+git clone https://github.com/raygarner/wallpaper.git /home/ray/pics/papes
+
+#download dotfiles
+git clone https://github.com/raygarner/dotfiles.git .
+
+#download and build dwm
+git clone https://github.com/raygarner/rays_dwm.git
+cd rays_dwm
+make
+sudo make install
+cd ..
+
+#download and build st
+git clone https://github.com/raygarner/st.git
+cd st
+make
+sudo make install
+cd ..
