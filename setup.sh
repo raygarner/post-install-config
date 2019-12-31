7f8d681abfe72069eef64076b90ff37e8cb22176
Ray Garner <ray@perfectcast.co.uk>
2019-12-31 20:56:25 +0000

    added ardour

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pacman -Syu
 
 #install packages
 echo "Installing packages..."
-pacman -S zathura zathura-pdf-mupdf make gvim gnu-free-fonts xorg firefox redshift youtube-dl feh mpv scrot xbindkeys htop sudo vi pulseaudio compton acpi git xorg-init xorg-xsetroot xorg-server dmenu libx11 freetype2 pkg-config transmission-cli base-devel kolourpaint neofetch xclip openssh unzip thunderbird perl-rename pulsemixer gdb ghc audacity musescore
+pacman -S zathura zathura-pdf-mupdf make gvim gnu-free-fonts xorg firefox redshift youtube-dl feh mpv scrot xbindkeys htop sudo vi pulseaudio compton acpi git xorg-init xorg-xsetroot xorg-server dmenu libx11 freetype2 pkg-config transmission-cli base-devel kolourpaint neofetch xclip openssh unzip thunderbird perl-rename pulsemixer gdb ghc audacity musescore ardour
 
 #set keyboard layout
 echo "Setting keyboard layout to gb..."
